acfd97e3931bc7a1241091f587a1c1030b5c0d60
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-07-15 01:16:35 +0900

    Create aoc2015/day3/day3_test.go

---
+++ src/AdventOfCode/aoc2015/day3/day3_test.go
@@ -0,0 +1,58 @@
+package day3
+
+import (
+	"reflect"
+	"testing"
+)
+
+/*************************************/
+/* Local function                    */
+/*************************************/
+
+// Compare two variables
+func equal(a, b interface{}) bool {
+	return reflect.DeepEqual(a, b)
+}
+
+/*************************************/
+/* Test                              */
+/*************************************/
+
+func TestMoveSata(t *testing.T) {
+	in := []string{
+		">",
+		"^>v<",
+		"^v^v^v^v",
+	}
+	want := [][]Point{
+		{
+			Point{0, 0},
+			Point{1, 0}, // >
+		},
+		{
+			Point{0, 0},
+			Point{0, 1}, // ^
+			Point{1, 1}, // >
+			Point{1, 0}, // v
+			Point{0, 0}, // <
+		},
+		{
+			Point{0, 0},
+			Point{0, 1}, // ^
+			Point{0, 0}, // v
+			Point{0, 1}, // ^
+			Point{0, 0}, // v
+			Point{0, 1}, // ^
+			Point{0, 0}, // v
+			Point{0, 1}, // ^
+			Point{0, 0}, // v
+		},
+	}
+
+	for k, v := range in {
+		got := MoveSanta(v)
+		if !equal(got, want[k]) {
+			t.Errorf("\ngot  = %v;\nwant = %v", got, want[k])
+		}
+	}
+}

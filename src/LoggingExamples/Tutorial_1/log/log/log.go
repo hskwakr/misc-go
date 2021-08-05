6e1e8cddbb3bc0d39a1f7d25eab7a838bc05dcb1
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-08-05 18:46:51 +0900

    Update src/LoggingExamples/Tutorial_1/log

---
+++ src/LoggingExamples/Tutorial_1/log/log/log.go
@@ -0,0 +1,24 @@
+package log
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	WarningLogger *log.Logger
+	InfoLogger    *log.Logger
+	ErrorLogger   *log.Logger
+)
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	InfoLogger = log.New(file, "INFO: ", log.LstdFlags|log.Lshortfile)
+	WarningLogger = log.New(file, "WARNING: ", log.LstdFlags|log.Lshortfile)
+	ErrorLogger = log.New(file, "ERROR: ", log.LstdFlags|log.Lshortfile)
+}
